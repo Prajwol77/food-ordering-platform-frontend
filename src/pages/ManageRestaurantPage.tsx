7796bf79fee9a571671b50abc2912c39beb5bea9
Prajwol <prajwolmahara777@gmail.com>
2024-05-30 20:58:51 +0545

    Search API was also created

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,17 +1,21 @@
 
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm.tsx";
-import {useCreateMyRestaurant, useGetMyRestaurant} from "@/api/MyRestaurantApi.tsx";
+import {useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant} from "@/api/MyRestaurantApi.tsx";
 
 const ManageRestaurantPage = () => {
-    const { createRestaurant, isLoading } = useCreateMyRestaurant();
+    const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
     const {restaurant} = useGetMyRestaurant();
-    console.log("🚀 ~ ManageRestaurantPage ~ restaurant:", restaurant)
+    // console.log("🚀 ~ ManageRestaurantPage ~ restaurant:", restaurant)
+    const {updateRestaurant, isLoading:isUpdateLoading} = useUpdateMyRestaurant();
+
+    const isEditing = !!restaurant;
+
     
     return (
         <ManageRestaurantForm
             restaurant = {restaurant?.data}
-            onSave={createRestaurant}
-            isLoading={isLoading}
+            onSave={isEditing ? updateRestaurant : createRestaurant}
+            isLoading={isCreateLoading || isUpdateLoading}
         />
     );
 };
