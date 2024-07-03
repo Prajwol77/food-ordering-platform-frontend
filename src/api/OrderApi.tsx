d9ac7ddc88992ea49621199344d938606bc10ed9
Prajwol <prajwolmahara777@gmail.com>
2024-07-03 21:13:41 +0545

    consume customers API needed

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -5,6 +5,8 @@ import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+export const useGetMyOrders = () => {};
+
 export type CheckoutSessionRequest = {
   cartItems: CartItem[];
   deliveryDetails: {
--- src/types.ts
+++ src/types.ts
@@ -1,57 +1,84 @@
 export type User = {
-    isAdmin?: boolean;
-    email:string;
-    name:string;
-    address:string;
-    city:string;
-    contact:string;
-    auth0Id: string;
-    _id: string;
+  isAdmin?: boolean;
+  email: string;
+  name: string;
+  address: string;
+  city: string;
+  contact: string;
+  auth0Id: string;
+  _id: string;
 };
 
 export type MenuItem = {
-    _id: string;
-    name: string;
-    price: number;
-}
+  _id: string;
+  name: string;
+  price: number;
+};
 
 export type Restaurant = {
-    _id: string;
-    user: string;
-    restaurantName: string;
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+  averageRating: number;
+  numberOfRatings: number;
+};
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    address: string;
     city: string;
-    deliveryPrice: number;
-    estimatedDeliveryTime: number;
-    cuisines: string[];
-    menuItems: MenuItem[];
-    imageUrl: string;
-    lastUpdated: string;
-    averageRating: number,
-    numberOfRatings: number
-}
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
+};
 
 export type RestaurantSearchResponse = {
-    data: Restaurant[];
-    pagination: {
-        total: number;
-        page: number;
-        pages: number;
-    }
-
-}
+  data: Restaurant[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
+  };
+};
 
 export type CommentSectionType = {
-    _id: string;
-    userId: CommentSectionForUserType;
-    restaurantID: string;
-    ratingValue: number;
-    comment: string;
-    createdAt: Date;
-    updatedAt: Date
-}
+  _id: string;
+  userId: CommentSectionForUserType;
+  restaurantID: string;
+  ratingValue: number;
+  comment: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 export type CommentSectionForUserType = {
-    _id: string;
-    name: string;
-    email: string
-}
+  _id: string;
+  name: string;
+  email: string;
+};
